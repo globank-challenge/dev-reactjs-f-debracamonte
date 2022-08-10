981072f6d563bce8b8715d3e14d4099dd930be15
fab-dbc <f.debracamonte@rabbitmx.com>
2022-08-10 16:19:19 -0500

    update useSetPokemonList hook

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,23 +4,19 @@ import Button from "../Button";
 import useSetPokemonList from "../../hooks/useSetPokemonList";
 
 const Footer = () => {
-  const {
-    previusPageHandler,
-    nextPageHandler,
-    itsPreviusDisabled,
-    itsNextDisabled,
-  } = useSetPokemonList();
+  const { previusPageHandler, nextPageHandler, itsDisabled } =
+    useSetPokemonList();
 
   return (
     <footer className="footer">
       <Button
         onClick={() => previusPageHandler()}
-        disabled={itsPreviusDisabled()}
+        disabled={itsDisabled("previous")}
       >
         <AiOutlineLeft />
-        {"Anterior"}
+        Anterior
       </Button>
-      <Button onClick={() => nextPageHandler()} disabled={itsNextDisabled()}>
+      <Button onClick={() => nextPageHandler()} disabled={itsDisabled("next")}>
         Siguiente
         <AiOutlineRight />
       </Button>
--- src/hooks/useSetPokemonList.ts
+++ src/hooks/useSetPokemonList.ts
@@ -7,34 +7,34 @@ import { getPokemonList } from "../services/getPokemonList.service";
 const useSetPokemonList = () => {
   const pokeList = useSelector((state: RootState) => state.pokemonList);
   const dispatch = useDispatch();
-  const itsNextDisabled = () => {
-    if (pokeList.next) return false;
+
+  const itsDisabled = (name: string) => {
+    let pokelink;
+    if (name === "next") pokelink = pokeList.next;
+    if (name === "previous") pokelink = pokeList.previus;
+    if (pokelink) return false;
     if (pokeList.loading) return false;
     return true;
   };
-  const itsPreviusDisabled = () => {
-    if (pokeList.previus) return false;
-    if (pokeList.loading) return false;
-    return true;
-  };
-  const nextPage = async (nextUrl: string) => {
-    const data = await getPokemonList(nextUrl);
-    dispatch(setCurrentList(data));
-  };
-  const previusPage = async (previusUrl: string) => {
-    const data = await getPokemonList(previusUrl);
-    dispatch(setCurrentList(data));
-  };
-  const nextPageHandler = () => {
+
+  const nextPageHandler = async () => {
     dispatch(setLoading(true));
-    if (pokeList.next) nextPage(pokeList.next);
+    if (pokeList.next) {
+      const data = await getPokemonList(pokeList.next);
+      dispatch(setCurrentList(data));
+    }
     dispatch(setLoading(false));
   };
-  const previusPageHandler = () => {
+
+  const previusPageHandler = async () => {
     dispatch(setLoading(true));
-    if (pokeList.previus) previusPage(pokeList.previus);
+    if (pokeList.previus) {
+      const data = await getPokemonList(pokeList.previus);
+      dispatch(setCurrentList(data));
+    }
     dispatch(setLoading(false));
   };
+
   const useInitPokemonList = () => {
     useEffect(() => {
       getPokemonList("https://pokeapi.co/api/v2/pokemon?offset=0&limit=4").then(
@@ -43,11 +43,11 @@ const useSetPokemonList = () => {
       dispatch(setLoading(false));
     }, []);
   };
+
   return {
     nextPageHandler,
     previusPageHandler,
-    itsNextDisabled,
-    itsPreviusDisabled,
+    itsDisabled,
     useInitPokemonList,
   };
 };
