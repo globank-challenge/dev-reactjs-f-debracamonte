fe04dfc1b9ca5de100b0235296d05fc5dc721367
fab-dbc <f.debracamonte@rabbitmx.com>
2022-08-12 13:23:25 -0500

    update DetailCard test

--- src/components/DetailCard/DetailCard.test.tsx
+++ src/components/DetailCard/DetailCard.test.tsx
@@ -1,17 +1,47 @@
 import { screen } from "@testing-library/react";
 import DetailCard from ".";
+import { setCurrentPokemon } from "../../redux/slices/pokemonDetails";
+import { setupStore } from "../../redux/store";
 import { render } from "../../utils/testUtils";
-
+import { fakePokemonDetailAdapted as mockData } from "../../utils/mockData";
 describe("<DetailCard />", () => {
-  test("should render labels", () => {
+  const store = setupStore();
+  store.dispatch(setCurrentPokemon(mockData));
+  test("should displays the type label", () => {
     render(<DetailCard />);
     const types = screen.getByText(/types/i);
-    const weigth = screen.getByText(/peso/i);
-    const img = screen.getByText(/sprite/i);
-    const movements = screen.getByText(/movimientos/i);
     expect(types).toBeInTheDocument();
+  });
+  test("should displays the correct type", () => {
+    render(<DetailCard />, { store });
+    const types = screen.getByText(mockData.types[0]);
+    expect(types).toBeInTheDocument();
+  });
+  test("should displays the weigth label", () => {
+    render(<DetailCard />);
+    const weigth = screen.getByText(/peso/i);
     expect(weigth).toBeInTheDocument();
+  });
+  test("should displays the correct weigth", () => {
+    render(<DetailCard />, { store });
+    const types = screen.getByText(`${mockData.weight}kg`);
+    expect(types).toBeInTheDocument();
+  });
+  test("should displays the img label", () => {
+    render(<DetailCard />);
+    const img = screen.getByText(/sprite/i);
     expect(img).toBeInTheDocument();
+  });
+  test("should displays the correct img", () => {
+    render(<DetailCard />, { store });
+    const types = screen.getByRole("img", {
+      name: `${mockData.name} front sprite`,
+    });
+    expect(types).toBeInTheDocument();
+  });
+  test("should displays the movements label", () => {
+    render(<DetailCard />);
+    const movements = screen.getByText(/movimientos/i);
     expect(movements).toBeInTheDocument();
   });
 });
--- src/services/getPokemonDetail.service.ts
+++ src/services/getPokemonDetail.service.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 import { pokemonDetailAdapter } from "../adapters/pokemonDetailAdapter";
+import { PokemonDetailAdapted } from "../adapters/types";
 
-export const getPokemonDetail = async (url: string) => {
+export const getPokemonDetail = async (
+  url: string
+): Promise<PokemonDetailAdapted | undefined> => {
   if (url === null) return;
   const data = await axios.get(url);
   return pokemonDetailAdapter(data.data);
