de88704b90bcab4aa515d6df8796dd068108ea04
fab-dbc <f.debracamonte@rabbitmx.com>
2022-08-12 17:16:10 -0500

    update button refact

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,12 +2,9 @@ import "./styles.css";
 import { ButtonProps } from "./types";
 
 const Button = ({ children, disabled, onClick }: ButtonProps) => {
-  const clickHandler = () => {
-    onClick();
-  };
   return (
     <button
-      onClick={clickHandler}
+      onClick={() => onClick()}
       disabled={disabled}
       className={`button ${disabled && "button--disabled"}`}
     >
