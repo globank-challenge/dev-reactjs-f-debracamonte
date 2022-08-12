1d8c69ad1ab8c8ce93a103a6963bdacae5dd74b4
fab-dbc <f.debracamonte@rabbitmx.com>
2022-08-12 15:30:28 -0500

    update Header test

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -7,6 +7,13 @@ describe("<Header />", () => {
     const title = screen.getByRole("heading", { name: /listado de pokemon/i });
     expect(title).toBeInTheDocument();
   });
+  test("should render the subtitle", () => {
+    render(<Header />);
+    const title = screen.getByRole("heading", {
+      name: /Dale click a tu pokemon favorito y mira su información/i,
+    });
+    expect(title).toBeInTheDocument();
+  });
   test("should render the searchBar", () => {
     render(<Header />);
     const searchBar = screen.getByRole("textbox");
