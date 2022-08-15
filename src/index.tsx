26b74429237109267cf378d5b6dc9e808f9afb36
fab-dbc <f.debracamonte@rabbitmx.com>
2022-08-15 11:20:49 -0500

    update index

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
 import { Provider } from "react-redux";
 import { setupStore } from "./redux/store";
+import Home from "./Screens/Home";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <Provider store={setupStore()}>
     <React.StrictMode>
-      <App />
+      <Home />
     </React.StrictMode>
   </Provider>
 );
